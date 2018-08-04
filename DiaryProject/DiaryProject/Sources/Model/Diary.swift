55f9eb58baaee67d17203e320396775bc23e7233
Kim Jong Seo <whdtj1450@naver.com>
2018-08-04 11:39:25 +0900

    엔트리 추가 테스트 작성 및 구현

---
+++ DiaryProject/DiaryProject/Sources/Model/Diary.swift
@@ -0,0 +1,9 @@
+//
+//  Diary.swift
+//  DiaryProject
+//
+//  Created by 김종서 on 2018. 8. 4..
+//  Copyright © 2018년 김종서. All rights reserved.
+//
+
+import Foundation
---
+++ DiaryProject/DiaryProject/Sources/Shared/Identifiable.swift
@@ -0,0 +1,9 @@
+//
+//  Identifiable.swift
+//  DiaryProject
+//
+//  Created by 김종서 on 2018. 8. 4..
+//  Copyright © 2018년 김종서. All rights reserved.
+//
+
+import Foundation
