58ccece6557a4e26f9cc9e0c8400520c8585ff8a
Kim Jong Seo <whdtj1450@naver.com>
2018-08-14 15:20:43 +0900

    키보드 애니메이션 구현

--- DiaryProject/DiaryProject/Sources/Controllers/EntryViewController.swift
+++ DiaryProject/DiaryProject/Sources/Controllers/EntryViewController.swift
@@ -18,10 +18,67 @@ extension DateFormatter{
     }()
 }
 
+let code = """
+    class EntryViewController: UIViewController {
+        @IBOutlet weak var dateLabel: UILabel!
+        @IBOutlet weak var textView: UITextView!
+        @IBOutlet weak var button: UIButton!
+
+        let journal: Journal = InMemoryJournal()
+        private var editingEntry: Entry?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            textView.text = "Test"
+            dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
+            button.addTarget(self, action: #selector(saveEntry(_:)), for: .touchUpInside)
+        }
+
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            textView.becomeFirstResponder() // 키보드 보이기
+        }
+
+        @objc func saveEntry(_ sender: Any) {
+            if let editing = editingEntry{
+                editing.text = textView.text
+                journal.update(editing)
+            }else{
+                let entry: Entry = Entry(text: textView.text)
+                journal.add(entry)
+                editingEntry = entry
+            }
+            updateSubviews(for: false)
+        }
+
+        @objc func editEntry(_ sender: Any){
+            updateSubviews(for: true)
+        }
+
+        fileprivate func updateSubviews(for isEditing: Bool) {
+            if isEditing{
+                textView.isEditable = true
+                textView.becomeFirstResponder()
+
+                button.setTitle("저장", for: .normal)
+                button.addTarget(self, action: #selector(saveEntry(_:)), for: .touchUpInside)
+            }else{
+                textView.isEditable = false // 저장 후 수정불가
+                textView.resignFirstResponder() // 키보드 숨기기
+
+                button.setTitle("수정", for: .normal)
+                button.addTarget(self, action: #selector(editEntry(_:)), for: .touchUpInside)
+            }
+        }
+    }
+"""
+
 class EntryViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
     
     let journal: Journal = InMemoryJournal()
     private var editingEntry: Entry?
@@ -29,10 +86,41 @@ class EntryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textView.text = "Test"
+        textView.text = code
         dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
         
         button.addTarget(self, action: #selector(saveEntry(_:)), for: .touchUpInside)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handlekeyboardAppearance(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handlekeyboardAppearance(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func handlekeyboardAppearance(_ note: Notification){
+        guard
+            let userInfo = note.userInfo,
+            let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue),
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval),
+            let curve = (userInfo[UIKeyboardAnimationCurveUserInfoKey] as? UInt)
+            else{ return }
+        
+        //print("키보드 높이 : \(keyboardFrame.cgRectValue.height)")
+        
+        let isKeyboardWillShow: Bool = note.name == Notification.Name.UIKeyboardWillShow
+        let keyboardHeight = isKeyboardWillShow ?
+            keyboardFrame.cgRectValue.height
+            : 0
+        let animationOption = UIViewAnimationOptions.init(rawValue: curve)
+
+        UIView.animate(
+            withDuration: duration,
+            delay: 0.0,
+            options: animationOption,
+            animations: {
+                self.textViewBottomConstraint.constant = -keyboardHeight
+                self.view.layoutIfNeeded()
+            },
+            completion: nil
+        )
     }
     
     override func viewDidAppear(_ animated: Bool) {
