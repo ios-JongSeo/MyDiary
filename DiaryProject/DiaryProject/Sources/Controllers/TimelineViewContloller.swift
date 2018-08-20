2ef1399c377d10ac1a20b70b20def0d38a4b9aa5
Kim Jong Seo <whdtj1450@naver.com>
2018-08-20 15:22:44 +0900

    TimelineViewController를 추가

--- DiaryProject/DiaryProject/Sources/Controllers/TimelineViewContloller.swift
+++ DiaryProject/DiaryProject/Sources/Controllers/TimelineViewContloller.swift
@@ -27,23 +27,3 @@ class TimelineViewController: UIViewController {
         : "엔트리 없음"
     }
 }
-
-//class TimelineViewController: UIViewController {
-//    @IBOutlet weak var entryCountLabel: UILabel!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        title = "Journal"
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        let journal = InMemoryEntryRepository.shared
-//
-//        entryCountLabel.text = journal.numberOfEntries > 0
-//            ? "엔트리 갯수: \(journal.numberOfEntries)"
-//            : "엔트리 없음"
-//    }
-//}
